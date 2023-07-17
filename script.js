15b3d07656eacb6f10fd19b308bd9c88deec1c77
Kelly Ribeiro <kelly.ribeiro18@etec.sp.gov.br>
2023-07-17 09:29:12 -0300

    landing page Rocketseat

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  let html = document.documentElement
+  html.classList.toggle("light")
+  let img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
